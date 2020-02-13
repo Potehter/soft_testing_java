4e10b48dd3c3c938ef135aec1b5246a8ca226437
Харитонов Дмитрий Алексеевич <d.kharitonov@hq.qiwi.com>
2020-02-14 00:38:23 +0300

    task 2. Method inside class

--- sandbox/src/main/java/ru/stqa/sandbox/CheckDistance.java
+++ sandbox/src/main/java/ru/stqa/sandbox/CheckDistance.java
@@ -6,12 +6,8 @@ public class CheckDistance {
         Point p2 = new Point(2, 2);
         System.out.println("First point : " + p1.x + ", " + p1.y);
         System.out.println("Second point : " + p2.x + ", " + p2.y);
-        System.out.println("Distance: " + distance(p1, p2));
+        System.out.println("Distance: " + p1.distance(p2));
     }
 
-    public static double distance (Point p1, Point p2) {
-        //расстояние между точками в пространстве считаем по гипотенузе
-        return Math.sqrt(Math.pow((p1.x - p2.x), 2) +
-                Math.pow((p1.y - p2.y), 2));
-    }
+
 }
